7db05545fb785cf86dfbbe1fd74e1cb07857d93c
Long Tien <superkillerpro123@gmail.com>
2022-05-21 21:44:33 +0700

    cac form cho user

--- getchamp/forms.py
+++ getchamp/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
 
-
+#tao form danh cho cac trang lien quan den user
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
